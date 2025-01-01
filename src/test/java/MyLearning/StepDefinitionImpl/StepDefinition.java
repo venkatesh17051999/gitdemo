ae01842d8a63bd96b2d877ae75d0bcadf9bf655d
venky <venkateshesakkin@gmail.com>
2025-01-01 20:49:58 +0530

    removed

--- src/test/java/MyLearning/StepDefinitionImpl/StepDefinition.java
+++ src/test/java/MyLearning/StepDefinitionImpl/StepDefinition.java
@@ -1,11 +1,10 @@
 package MyLearning.StepDefinitionImpl;
 
+import MyLearning.BaseTest.BaseTestComponents;
+
 public class StepDefinition extends BaseTestComponents{
 	
-	@Given ("I landed on Ecommerce Page")
-	public class I_landed_on_Ecommerce_Page() {
-		launchPage();
-	}
+	
 	
 	
 }
